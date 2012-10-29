f2318d9960b56207c4a27ca711aac714f5900bda
Christopher Suter <cgs1019@gmail.com>
2012-10-28 19:04:13 -0700

    add comments to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+#
+# vimjdb main build script.
+#
+# running
+#  $ ./build.sh
+# will create things as detailed below.
+#
+# running
+#  $ ./build.sh clean
+# will delete anything this build script creates
+#
+# creates build environment and subsequently sources any build.sh files found in
+# directories that are adjacent to this build.sh file. note that they are
+# sourced as part of this shell and not run in separate shells. any failures
+# should kill the build and cause non-zero exit status to be returned.
+#
+# build environment consists of:
+#  build-log/ - directory for build logs
+#  build-gen/ - intermediate place for generated code and such
+#  build-bin/ - actual, ostensibly distributable stuff
+#  symlinks to latest build stdout/stderr logs
+#
+# see submodule build.sh files for details on what goes on there
 
 dir=$(dirname "$0")
 dir=$(cd "$dir" && pwd)

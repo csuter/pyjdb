7820ee7e7bbb05dc9c7eed23d1dc4e60750ac876
Christopher Suter <cgs1019@gmail.com>
2012-10-28 17:53:27 -0700

    add build.sh clean option. rename build logs more precisely

--- build.sh
+++ build.sh
@@ -16,21 +16,30 @@ build_bin_dir="$dir/build-bin"
 build_gen_dir="$dir/build-gen"
 build_log_dir="$dir/build-log"
 
+if [[ $1 == 'clean' ]]
+then
+  rm -rf "$build_bin_dir"
+  rm -rf "$build_gen_dir"
+  rm -rf "$build_log_dir"
+  exit 0
+fi
+
 rm -rf "$build_bin_dir/"*
 rm -rf "$build_gen_dir/"*
 
 mkdir -p "$build_bin_dir"
 mkdir -p "$build_gen_dir"
 mkdir -p "$build_log_dir"
+mkdir -p tmp
 
 stderr_log="$build_log_dir/$timestamp.build.sh.err"
 stdout_log="$build_log_dir/$timestamp.build.sh.out"
-last_err=last_err
-last_out=last_out
+latest_build_out=latest_build.out
+latest_build_err=latest_build.err
 exec > >(tee "$stdout_log")
 exec 2> >(tee "$stderr_log")
-ln -sf "$stderr_log" "$last_err"
-ln -sf "$stdout_log" "$last_out"
+ln -sf "$stderr_log" "$latest_build.err"
+ln -sf "$stdout_log" "$latest_build_out"
 
 log "Logging to $stdout_log and $stderr_log"
 

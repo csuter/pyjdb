2e5514724f9cc4a6564c68884fdb7fa49ff02aea
Christopher Suter <cgs1019@gmail.com>
2012-10-28 18:22:14 -0700

    correct indenting; use 2 spaces no tabs

--- jdwprpc/codegen/pyserver_structures.py
+++ jdwprpc/codegen/pyserver_structures.py
@@ -1,142 +1,142 @@
 import spec_structures
 
 def py_server_impl(spec):
-	return "\n".join([
-		"import ".join(["import jdwp_pb2\n", "jdwp_impl\n", "struct\n", "google.protobuf.message\n"]),
-		server_constants(spec.command_sets_),
-		"\n".join([ command_set_impl(cs) for cs in spec.command_sets_ ])])
+  return "\n".join([
+    "import ".join(["import jdwp_pb2\n", "jdwp_impl\n", "struct\n", "google.protobuf.message\n"]),
+    server_constants(spec.command_sets_),
+    "\n".join([ command_set_impl(cs) for cs in spec.command_sets_ ])])
 
 def server_constants(command_sets):
-	return "\n".join([
-			"COMMAND_SPECS = dict()",
-			"\n".join(["\n".join([
-					"COMMAND_SPECS[\"%s_%s\"] = (%s,%s,\"%s\",\"%s\")" % (
-							command_set.name_, command.name_,
-							command_set.id_, command.id_,
-							command.request_.pack_fmt(),
-							command.response_.pack_fmt()
-							)
-					for command in command_set.commands_ ])
-			for command_set in command_sets ])])
+  return "\n".join([
+      "COMMAND_SPECS = dict()",
+      "\n".join(["\n".join([
+          "COMMAND_SPECS[\"%s_%s\"] = (%s,%s,\"%s\",\"%s\")" % (
+              command_set.name_, command.name_,
+              command_set.id_, command.id_,
+              command.request_.pack_fmt(),
+              command.response_.pack_fmt()
+              )
+          for command in command_set.commands_ ])
+      for command_set in command_sets ])])
 
 def command_set_impl(cs):
-	return "\n".join([
-		"class %sImpl(jdwp_pb2.%s):" % (cs.name_, cs.name_),
-		"\tdef __init__(self, jdwp):",
-		"\t\tself.jdwp_ = jdwp",
-		"\n".join([ "%s" % command_impl(cs, cmd) for cmd in cs.commands_ ])
-		])
+  return "\n".join([
+    "class %sImpl(jdwp_pb2.%s):" % (cs.name_, cs.name_),
+    "\tdef __init__(self, jdwp):",
+    "\t\tself.jdwp_ = jdwp",
+    "\n".join([ "%s" % command_impl(cs, cmd) for cmd in cs.commands_ ])
+    ])
 
 def command_impl(cs, cmd):
-	return "\n".join([
-		"\tdef %s_%s(self, controller, request, done):" % (cs.name_, cmd.name_),
-		request_packing_impl(cs, cmd, cmd.request_),
-		jdwp_call_impl(cs, cmd),
-		{
-			spec_structures.Response:response_unpacking_impl,
-			spec_structures.Event:event_unpacking_impl
-			}[type(cmd.response_)](cs, cmd, cmd.response_),
-		"\t\tdone.run(response)",
-		])
+  return "\n".join([
+    "\tdef %s_%s(self, controller, request, done):" % (cs.name_, cmd.name_),
+    request_packing_impl(cs, cmd, cmd.request_),
+    jdwp_call_impl(cs, cmd),
+    {
+      spec_structures.Response:response_unpacking_impl,
+      spec_structures.Event:event_unpacking_impl
+      }[type(cmd.response_)](cs, cmd, cmd.response_),
+    "\t\tdone.run(response)",
+    ])
 
 def request_packing_impl(cs, cmd, request):
-	return "\n".join([
-			"\t\tdata = []",
-			"\n".join([
-					request_arg_packing_impl("data", cs, cmd, arg, idx)
-					for (idx, arg) in enumerate(request.args_) ]),
-			])
+  return "\n".join([
+      "\t\tdata = []",
+      "\n".join([
+          request_arg_packing_impl("data", cs, cmd, arg, idx)
+          for (idx, arg) in enumerate(request.args_) ]),
+      ])
 
 def jdwp_call_impl(cs, cmd):
-	return "\n".join([
-			"\t\treply = self.jdwp_.call(\"%s_%s\", %s, %s, data)" % (
-					cs.name_, cmd.name_, cs.id_, cmd.id_),
-			])
+  return "\n".join([
+      "\t\treply = self.jdwp_.call(\"%s_%s\", %s, %s, data)" % (
+          cs.name_, cmd.name_, cs.id_, cmd.id_),
+      ])
 
 def response_unpacking_impl(cs, cmd, response):
-	return "\n".join([
-			"\t\tresponse = jdwp_pb2.%s_%s_Response()" % (cs.name_, cmd.name_),
-			"\n".join([
-					response_arg_unpacking_impl("reply[%s]" % idx, cs, cmd, arg, idx)
-					for (idx, arg) in enumerate(response.args_) ]),
-			])
+  return "\n".join([
+      "\t\tresponse = jdwp_pb2.%s_%s_Response()" % (cs.name_, cmd.name_),
+      "\n".join([
+          response_arg_unpacking_impl("reply[%s]" % idx, cs, cmd, arg, idx)
+          for (idx, arg) in enumerate(response.args_) ]),
+      ])
 
 def event_unpacking_impl(cs, cmd, response):
-	return "\n".join([
-			#"\t\tresponse = jdwp_pb2.%s_%s_Response()" % (cs.name_, cmd.name_),
-			#"\n".join([
-			#		response_arg_unpacking_impl("reply[%s]" % idx, cs, cmd, arg, idx)
-			#		for (idx, arg) in enumerate(response.args_) ]),
-			])
+  return "\n".join([
+      #"\t\tresponse = jdwp_pb2.%s_%s_Response()" % (cs.name_, cmd.name_),
+      #"\n".join([
+      #    response_arg_unpacking_impl("reply[%s]" % idx, cs, cmd, arg, idx)
+      #    for (idx, arg) in enumerate(response.args_) ]),
+      ])
 
 def request_arg_packing_impl(data, cs, cmd, arg, idx):
-	return {
-			spec_structures.Simple:simple_request_arg_unpacking_impl,
-			spec_structures.Repeat:repeat_request_arg_unpacking_impl,
-			}[type(arg)](data, cs, cmd, arg, idx)
+  return {
+      spec_structures.Simple:simple_request_arg_unpacking_impl,
+      spec_structures.Repeat:repeat_request_arg_unpacking_impl,
+      }[type(arg)](data, cs, cmd, arg, idx)
 
 def simple_request_arg_unpacking_impl(data, cs, cmd, arg, idx):
-	return "\t\tdata.append(request.%s)" % arg.name_
+  return "\t\tdata.append(request.%s)" % arg.name_
 
 def repeat_request_arg_unpacking_impl(data, cs, cmd, arg, idx):
-	return { spec_structures.Simple:simple_repeat_request_arg_unpacking_impl,
-		spec_structures.Group:group_repeat_request_arg_unpacking_impl,
-		spec_structures.Select:select_repeat_request_arg_unpacking_impl
-		}[type(arg.arg_)](data, cs, cmd, arg, idx)
+  return { spec_structures.Simple:simple_repeat_request_arg_unpacking_impl,
+    spec_structures.Group:group_repeat_request_arg_unpacking_impl,
+    spec_structures.Select:select_repeat_request_arg_unpacking_impl
+    }[type(arg.arg_)](data, cs, cmd, arg, idx)
 
 def simple_repeat_request_arg_unpacking_impl(data, cs, cmd, arg, idx):
-	return "\n".join([
-		"\t\tdata.append(request.%s)" % arg.name_,
-		])
+  return "\n".join([
+    "\t\tdata.append(request.%s)" % arg.name_,
+    ])
 
 def group_repeat_request_arg_unpacking_impl(data, cs, cmd, arg, idx):
-	group = arg.arg_
-	return "\n".join([
-			"\t\tfor item in %s:" % data,
-			"\t\t\tnew_item = response.%s.add()" % arg.name_,
-			"\n".join([
-					"\t\t\tnew_item.%s = item[%s]" % (sub_arg.name_, idx)
-							for (idx, sub_arg) in enumerate(group.args_) ])
-			])
+  group = arg.arg_
+  return "\n".join([
+      "\t\tfor item in %s:" % data,
+      "\t\t\tnew_item = response.%s.add()" % arg.name_,
+      "\n".join([
+          "\t\t\tnew_item.%s = item[%s]" % (sub_arg.name_, idx)
+              for (idx, sub_arg) in enumerate(group.args_) ])
+      ])
 
 def select_repeat_request_arg_unpacking_impl(data, cs, cmd, arg, idx):
-	select = arg.arg_
-	return "\n".join([
-			"\t\tselect_repeat = []",
-			"\t\tfor item in request.%s:" % arg.name_,
-			"\t\t\tselect_repeat.append(jdwp_impl.proto_to_data(item))",
-			"\t\tdata.append(select_repeat)",
-			])
+  select = arg.arg_
+  return "\n".join([
+      "\t\tselect_repeat = []",
+      "\t\tfor item in request.%s:" % arg.name_,
+      "\t\t\tselect_repeat.append(jdwp_impl.proto_to_data(item))",
+      "\t\tdata.append(select_repeat)",
+      ])
 
 def response_arg_unpacking_impl(data, cs, cmd, arg, idx):
-	return {
-			spec_structures.Simple:simple_response_arg_unpacking_impl,
-			spec_structures.Repeat:repeat_response_arg_unpacking_impl,
-			}[type(arg)](data, cs, cmd, arg, idx)
+  return {
+      spec_structures.Simple:simple_response_arg_unpacking_impl,
+      spec_structures.Repeat:repeat_response_arg_unpacking_impl,
+      }[type(arg)](data, cs, cmd, arg, idx)
 
 def simple_response_arg_unpacking_impl(data, cs, cmd, arg, idx):
-	return "\t\tresponse.%s = %s" % (arg.name_, data)
+  return "\t\tresponse.%s = %s" % (arg.name_, data)
 
 def repeat_response_arg_unpacking_impl(data, cs, cmd, arg, idx):
-	return { spec_structures.Simple:simple_repeat_response_arg_unpacking_impl,
-		spec_structures.Group:group_repeat_response_arg_unpacking_impl,
-		spec_structures.Select:select_repeat_response_arg_unpacking_impl
-		}[type(arg.arg_)](data, cs, cmd, arg, idx)
+  return { spec_structures.Simple:simple_repeat_response_arg_unpacking_impl,
+    spec_structures.Group:group_repeat_response_arg_unpacking_impl,
+    spec_structures.Select:select_repeat_response_arg_unpacking_impl
+    }[type(arg.arg_)](data, cs, cmd, arg, idx)
 
 def simple_repeat_response_arg_unpacking_impl(data, cs, cmd, arg, idx):
-	return "\n".join([
-		"\t\tresponse.%s.extend([ i[0] for i in %s ])" % (arg.name_, data),
-		])
+  return "\n".join([
+    "\t\tresponse.%s.extend([ i[0] for i in %s ])" % (arg.name_, data),
+    ])
 
 def group_repeat_response_arg_unpacking_impl(data, cs, cmd, arg, idx):
-	group = arg.arg_
-	return "\n".join([
-			"\t\tfor item in %s:" % data,
-			"\t\t\tnew_item = response.%s.add()" % arg.name_,
-			"\n".join([
-					"\t\t\tnew_item.%s = item[%s]" % (sub_arg.name_, idx)
-							for (idx, sub_arg) in enumerate(group.args_) ])
-			])
+  group = arg.arg_
+  return "\n".join([
+      "\t\tfor item in %s:" % data,
+      "\t\t\tnew_item = response.%s.add()" % arg.name_,
+      "\n".join([
+          "\t\t\tnew_item.%s = item[%s]" % (sub_arg.name_, idx)
+              for (idx, sub_arg) in enumerate(group.args_) ])
+      ])
 
 def select_repeat_response_arg_unpacking_impl(data, cs, cmd, arg, idx):
-	return "\t\tresponse.%s = %s" % (arg.name_, data)
+  return "\t\tresponse.%s = %s" % (arg.name_, data)
--- jdwprpc/jdwp_impl/jdwp_impl.py
+++ jdwprpc/jdwp_impl/jdwp_impl.py
@@ -1,299 +1,298 @@
 import google.protobuf.descriptor
 import jdwprpc
-import logging
 import socket
 import struct
 import threading
 import time
 
 class Jdwp:
-	def __init__(self, port = 5005):
-		self.establish_connection(port)
-		# dict of req_id -> (cmd_set, cmd)
-		self.requests = dict()
-		# dict of req_id -> reply
-		self.replies = dict()
-		# request ids are simply created sequentially starting with 0
-		self.next_req_id = 0
+  def __init__(self, port = 5005):
+    self.establish_connection(port)
+    # dict of req_id -> (cmd_set, cmd)
+    self.requests = dict()
+    # dict of req_id -> reply
+    self.replies = dict()
+    # request ids are simply created sequentially starting with 0
+    self.next_req_id = 0
 
-		# Initialize event listener
-		self.reader_thread_ = EventListenerThread()
-		self.reader_thread_.jdwp_ = self
-		self.reader_thread_.start()
+    # Initialize event listener
+    self.reader_thread_ = EventListenerThread()
+    self.reader_thread_.jdwp_ = self
+    self.reader_thread_.start()
 
-	def establish_connection(self, port):
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		retries = 5
-		while retries > 0:
-			retries -= 1
-			try:
-				self.sock.connect(('localhost', port))
-				break
-			except Exception, e:
-				logging.exception(e)
-				logging.error("Connection failed. Retrying %d times" % retries)
-				time.sleep(1)
-		# handshake
-		self.sock.send(b'JDWP-Handshake')
-		data = self.sock.recv(14)
-		if data != b'JDWP-Handshake':
-			raise Exception('Failed handshake')
+  def establish_connection(self, port):
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    retries = 5
+    while retries > 0:
+      retries -= 1
+      try:
+        self.sock.connect(('localhost', port))
+        break
+      except Exception, e:
+        print("Exception: %s" % e)
+        print("Connection failed. Retrying %d times" % retries)
+        time.sleep(1)
+    # handshake
+    self.sock.send(b'JDWP-Handshake')
+    data = self.sock.recv(14)
+    if data != b'JDWP-Handshake':
+      raise Exception('Failed handshake')
 
-	def call(self, name, cmd_set, cmd, data):
-		return self.pop_reply(self.call_async(name, cmd_set, cmd, data))
+  def call(self, name, cmd_set, cmd, data):
+    return self.pop_reply(self.call_async(name, cmd_set, cmd, data))
 
-	def call_async(self, name, cmd_set, cmd, data):
-		req_id = self.next_id()
-		self.requests[req_id] = name
-		packed_data = pack_jdi_data(jdwprpc.COMMAND_SPECS[name][2], data)
-		self.send(req_id, cmd_set, cmd, packed_data)
-		return req_id
+  def call_async(self, name, cmd_set, cmd, data):
+    req_id = self.next_id()
+    self.requests[req_id] = name
+    packed_data = pack_jdi_data(jdwprpc.COMMAND_SPECS[name][2], data)
+    self.send(req_id, cmd_set, cmd, packed_data)
+    return req_id
 
-	def get_reply(self, req_id):
-		if req_id not in self.replies:
-			while req_id not in self.replies:
-				1
-		if self.requests[req_id] in jdwprpc.COMMAND_SPECS:
-			key = self.requests[req_id]
-			_, err, data = self.replies[req_id]
-			if err != 0:
-				raise Exception("JDWP Error(%s=%d): \"%s\"" % (
-						ERROR_MESSAGES[err][0], err, ERROR_MESSAGES[err][1]))
-			return unpack_jdi_data(jdwprpc.COMMAND_SPECS[key][3], data)[0]
-		return []
+  def get_reply(self, req_id):
+    if req_id not in self.replies:
+      while req_id not in self.replies:
+        1
+    if self.requests[req_id] in jdwprpc.COMMAND_SPECS:
+      key = self.requests[req_id]
+      _, err, data = self.replies[req_id]
+      if err != 0:
+        raise Exception("JDWP Error(%s=%d): \"%s\"" % (
+            ERROR_MESSAGES[err][0], err, ERROR_MESSAGES[err][1]))
+      return unpack_jdi_data(jdwprpc.COMMAND_SPECS[key][3], data)[0]
+    return []
 
-	def pop_reply(self, req_id):
-		result = self.get_reply(req_id)
-		del self.replies[req_id]
-		del self.requests[req_id]
-		return result
+  def pop_reply(self, req_id):
+    result = self.get_reply(req_id)
+    del self.replies[req_id]
+    del self.requests[req_id]
+    return result
 
-	def send(self, req_id, cmdset, cmd, data):
-		flags = 0
-		length = 11 + len(data)
-		header = struct.pack(">IIBBB", length, req_id, flags, cmdset, cmd)
-		self.sock.send(header)
-		self.sock.send(data)
+  def send(self, req_id, cmdset, cmd, data):
+    flags = 0
+    length = 11 + len(data)
+    header = struct.pack(">IIBBB", length, req_id, flags, cmdset, cmd)
+    self.sock.send(header)
+    self.sock.send(data)
 
-	def recv(self):
-		header = read_all(self.sock, 11)
-		length, req_id, flags, err = struct.unpack('>IIBH', header)
-		remaining = length - 11
-		data = read_all(self.sock, remaining)
-		return req_id, flags, err, data
+  def recv(self):
+    header = read_all(self.sock, 11)
+    length, req_id, flags, err = struct.unpack('>IIBH', header)
+    remaining = length - 11
+    data = read_all(self.sock, remaining)
+    return req_id, flags, err, data
 
-	def next_id(self):
-		self.next_req_id += 1
-		return self.next_req_id
+  def next_id(self):
+    self.next_req_id += 1
+    return self.next_req_id
 
 
 def read_all(sock, num_bytes):
-	msgparts = []
-	remaining = num_bytes
-	while remaining > 0:
-		chunk = sock.recv(remaining)
-		msgparts.append(chunk)
-		remaining -= len(chunk)
-	return b''.join(msgparts)
+  msgparts = []
+  remaining = num_bytes
+  while remaining > 0:
+    chunk = sock.recv(remaining)
+    msgparts.append(chunk)
+    remaining -= len(chunk)
+  return b''.join(msgparts)
 
 
 def unpack_jdi_data(fmt, data):
-	result = []
-	pos = 0
-	in_paren = 0
-	size = 0
-	idx = 0
-	while idx < len(fmt):
-		c = fmt[idx]
-		if in_paren > 0:
-			if c == '(':
-				in_paren += 1
-			elif c == ')':
-				in_paren -= 1
-			idx += 1
-			continue
-		if c == 'S':
-			strlen = struct.unpack(">I", data[pos:pos+4])[0]
-			result.append(struct.unpack(str(strlen) + "s",
-					data[pos+4:pos+strlen+4])[0].decode('UTF-8'))
-			size += 4 + strlen
-			pos += 4 + strlen
-		elif c == 'I':
-			result.append(struct.unpack(">I", data[pos:pos+4])[0])
-			size += 4
-			pos += 4
-		elif c == 'b':
-			result.append(struct.unpack(">B", data[pos:pos+1])[0] != 0)
-			size += 1
-			pos += 1
-		elif c == 'B':
-			result.append(struct.unpack(">B", data[pos:pos+1])[0])
-			size += 1
-			pos += 1
-		elif c == 'L':
-			result.append(struct.unpack(">Q", data[pos:pos+8])[0])
-			size += 8
-			pos += 8
-		elif c == 'V':
-			value, value_size = parse_jdi_value(data)
-			result.append(value)
-			size += value_size
-			pos += value_size
-		elif c == 'T':
-			type_tag, object_id = struct.unpack(">BQ", data[pos:pos+9])
-			result.append((type_tag, object_id))
-			size += 9
-			pos += 9
-		elif c == 'X':
-			type_tag, class_id, method_id, index = struct.unpack(">BQQQ", data[pos:pos+25])
-			result.append((type_tag, class_id, method_id, index))
-			size += 25
-			pos += 25
-		elif c == 'A':
-			raise Exception("IMPLEMENT ARRAY REGION UNPACKING")
-		#elif c == '?':
-		elif c == 'R':
-			num = struct.unpack(">I", data[pos:pos+4])[0]
-			pos += 4
-			sub_result = []
-			if fmt[idx+1] != '(':
-				raise Exception("jdi_data fmt exception: expect '(' after 'R'")
-			close_paren = find_close_paren(fmt, idx+1)
-			if close_paren == -1:
-				raise Exception("jdi_data fmt exception: no matching ')'")
-			sub_data_fmt = fmt[idx+2:close_paren]
-			for i in range(num):
-				sub_data, sub_size = unpack_jdi_data(sub_data_fmt, data[pos:])
-				pos += sub_size
-				size += sub_size
-				sub_result.append(sub_data)
-			result.append(sub_result)
-		elif c == '(':
-			in_paren = 1
-		idx += 1
-	return result, size
+  result = []
+  pos = 0
+  in_paren = 0
+  size = 0
+  idx = 0
+  while idx < len(fmt):
+    c = fmt[idx]
+    if in_paren > 0:
+      if c == '(':
+        in_paren += 1
+      elif c == ')':
+        in_paren -= 1
+      idx += 1
+      continue
+    if c == 'S':
+      strlen = struct.unpack(">I", data[pos:pos+4])[0]
+      result.append(struct.unpack(str(strlen) + "s",
+          data[pos+4:pos+strlen+4])[0].decode('UTF-8'))
+      size += 4 + strlen
+      pos += 4 + strlen
+    elif c == 'I':
+      result.append(struct.unpack(">I", data[pos:pos+4])[0])
+      size += 4
+      pos += 4
+    elif c == 'b':
+      result.append(struct.unpack(">B", data[pos:pos+1])[0] != 0)
+      size += 1
+      pos += 1
+    elif c == 'B':
+      result.append(struct.unpack(">B", data[pos:pos+1])[0])
+      size += 1
+      pos += 1
+    elif c == 'L':
+      result.append(struct.unpack(">Q", data[pos:pos+8])[0])
+      size += 8
+      pos += 8
+    elif c == 'V':
+      value, value_size = parse_jdi_value(data)
+      result.append(value)
+      size += value_size
+      pos += value_size
+    elif c == 'T':
+      type_tag, object_id = struct.unpack(">BQ", data[pos:pos+9])
+      result.append((type_tag, object_id))
+      size += 9
+      pos += 9
+    elif c == 'X':
+      type_tag, class_id, method_id, index = struct.unpack(">BQQQ", data[pos:pos+25])
+      result.append((type_tag, class_id, method_id, index))
+      size += 25
+      pos += 25
+    elif c == 'A':
+      raise Exception("IMPLEMENT ARRAY REGION UNPACKING")
+    #elif c == '?':
+    elif c == 'R':
+      num = struct.unpack(">I", data[pos:pos+4])[0]
+      pos += 4
+      sub_result = []
+      if fmt[idx+1] != '(':
+        raise Exception("jdi_data fmt exception: expect '(' after 'R'")
+      close_paren = find_close_paren(fmt, idx+1)
+      if close_paren == -1:
+        raise Exception("jdi_data fmt exception: no matching ')'")
+      sub_data_fmt = fmt[idx+2:close_paren]
+      for i in range(num):
+        sub_data, sub_size = unpack_jdi_data(sub_data_fmt, data[pos:])
+        pos += sub_size
+        size += sub_size
+        sub_result.append(sub_data)
+      result.append(sub_result)
+    elif c == '(':
+      in_paren = 1
+    idx += 1
+  return result, size
 
 
 def parse_jdi_value(data):
-	tag = struct.unpack(">B", data[0])
-	data_size = tag_constants_data_sizes[tag]
-	fmt = ">" + "B" * data_size
-	data = struct.unpack(fmt, data[1:])
-	return (tag, data), 1 + data_size
+  tag = struct.unpack(">B", data[0])
+  data_size = tag_constants_data_sizes[tag]
+  fmt = ">" + "B" * data_size
+  data = struct.unpack(fmt, data[1:])
+  return (tag, data), 1 + data_size
 
 
 def find_close_paren(string, start):
-	count = 1
-	if string[start] == '(':
-		idx = start + 1
-	else:
-		idx = start
-	while count > 0:
-		if string[idx] == '(':
-			count += 1
-		elif string[idx] == ')':
-			count -= 1
-		idx += 1
-	return idx-1
+  count = 1
+  if string[start] == '(':
+    idx = start + 1
+  else:
+    idx = start
+  while count > 0:
+    if string[idx] == '(':
+      count += 1
+    elif string[idx] == ')':
+      count -= 1
+    idx += 1
+  return idx-1
 
 
 def pack_jdi_data(fmt, data):
-	result = bytearray()
-	pos = 0
-	in_paren = 0
-	idx = 0
-	while idx < len(fmt):
-		c = fmt[idx]
-		if in_paren > 0:
-			if c == '(':
-				in_paren += 1
-			elif c == ')':
-				in_paren -= 1
-			idx += 1
-			continue
-		elif c == 'B':
-			# write string length
-			result.extend(struct.pack(">B", data[pos]))
-			pos += 1
-		elif c == 'b':
-			# write string length
-			result.extend(struct.pack(">B", data[pos]))
-			pos += 1
-		elif c == 'I':
-			# write string length
-			result.extend(struct.pack(">I", data[pos]))
-			pos += 1
-		elif c == 'L':
-			# write string length
-			result.extend(struct.pack(">Q", data[pos]))
-			pos += 1
-		elif c == 'S':
-			strlen = len(data[pos])
-			# write string length
-			result.extend(struct.pack(">I", strlen))
-			result.extend(bytearray(data[pos][0],"UTF-8"))
-			pos += 1
-		elif c == 'A':
-			raise Exception("IMPLEMENT ARRAY REGION PACKING")
-		elif c == '?':
-			type_tag, rest = data
-			type_tag_fmt = fmt[idx+1:idx+2]
-			result.extend(pack_jdi_data(type_tag_fmt, type_tag))
-			sub_data_fmt = get_paren_substr_after(fmt, idx+1)
-			clauses = dict((int(k), v) for (k, v) in (x.split("=") for x in sub_data_fmt.split("|")))
-			result.extend(pack_jdi_data(clauses[type_tag[0]], rest))
-			idx += 1
-			pos += 1
-		elif c == 'R':
-			num = len(data[pos])
-			result.extend(struct.pack(">I", num))
-			sub_result = bytearray()
-			sub_data_fmt = get_paren_substr_after(fmt, idx)
-			for i in range(num):
-				sub_data = pack_jdi_data(sub_data_fmt, data[pos][i])
-				sub_result.extend(sub_data)
-			pos += 1
-			result.extend(sub_result)
-		elif c == '(':
-			in_paren = 1
-		else:
-			raise Exception("Unrecognized fmt char %s at idx %s in fmt \"%s\" for data \"%s\"" % (
-					c, idx, fmt, data))
-		idx += 1
-	return result
+  result = bytearray()
+  pos = 0
+  in_paren = 0
+  idx = 0
+  while idx < len(fmt):
+    c = fmt[idx]
+    if in_paren > 0:
+      if c == '(':
+        in_paren += 1
+      elif c == ')':
+        in_paren -= 1
+      idx += 1
+      continue
+    elif c == 'B':
+      # write string length
+      result.extend(struct.pack(">B", data[pos]))
+      pos += 1
+    elif c == 'b':
+      # write string length
+      result.extend(struct.pack(">B", data[pos]))
+      pos += 1
+    elif c == 'I':
+      # write string length
+      result.extend(struct.pack(">I", data[pos]))
+      pos += 1
+    elif c == 'L':
+      # write string length
+      result.extend(struct.pack(">Q", data[pos]))
+      pos += 1
+    elif c == 'S':
+      strlen = len(data[pos])
+      # write string length
+      result.extend(struct.pack(">I", strlen))
+      result.extend(bytearray(data[pos][0],"UTF-8"))
+      pos += 1
+    elif c == 'A':
+      raise Exception("IMPLEMENT ARRAY REGION PACKING")
+    elif c == '?':
+      type_tag, rest = data
+      type_tag_fmt = fmt[idx+1:idx+2]
+      result.extend(pack_jdi_data(type_tag_fmt, type_tag))
+      sub_data_fmt = get_paren_substr_after(fmt, idx+1)
+      clauses = dict((int(k), v) for (k, v) in (x.split("=") for x in sub_data_fmt.split("|")))
+      result.extend(pack_jdi_data(clauses[type_tag[0]], rest))
+      idx += 1
+      pos += 1
+    elif c == 'R':
+      num = len(data[pos])
+      result.extend(struct.pack(">I", num))
+      sub_result = bytearray()
+      sub_data_fmt = get_paren_substr_after(fmt, idx)
+      for i in range(num):
+        sub_data = pack_jdi_data(sub_data_fmt, data[pos][i])
+        sub_result.extend(sub_data)
+      pos += 1
+      result.extend(sub_result)
+    elif c == '(':
+      in_paren = 1
+    else:
+      raise Exception("Unrecognized fmt char %s at idx %s in fmt \"%s\" for data \"%s\"" % (
+          c, idx, fmt, data))
+    idx += 1
+  return result
 
 def proto_to_data(proto):
-	fields = []
-	if hasattr(proto, '_fields'):
-		for field in proto._fields:
-			value = proto._fields[field]
-			if field.label == 3:
-				data = [ proto_to_data(entry) for entry in value ]
-			else:
-				data = proto_to_data(value)
-			fields.append((field.number, data))
-		fields = [ entry[1] for entry in sorted(fields, key = lambda k:k[0]) ]
-	else:
-		fields = [ proto ]
-	return fields
+  fields = []
+  if hasattr(proto, '_fields'):
+    for field in proto._fields:
+      value = proto._fields[field]
+      if field.label == 3:
+        data = [ proto_to_data(entry) for entry in value ]
+      else:
+        data = proto_to_data(value)
+      fields.append((field.number, data))
+    fields = [ entry[1] for entry in sorted(fields, key = lambda k:k[0]) ]
+  else:
+    fields = [ proto ]
+  return fields
 
 def get_paren_substr_after(fmt, idx):
-	if fmt[idx+1] != '(':
-		raise Exception("jdi_data fmt exception: expected '(' at %d of '%s'" % (idx, fmt))
-	close_paren = find_close_paren(fmt, idx+1)
-	if close_paren == -1:
-		raise Exception("jdi_data fmt exception: no matching ')' for paren at %d of '%s'" % (idx, fmt))
-	return fmt[idx+2:close_paren]
+  if fmt[idx+1] != '(':
+    raise Exception("jdi_data fmt exception: expected '(' at %d of '%s'" % (idx, fmt))
+  close_paren = find_close_paren(fmt, idx+1)
+  if close_paren == -1:
+    raise Exception("jdi_data fmt exception: no matching ')' for paren at %d of '%s'" % (idx, fmt))
+  return fmt[idx+2:close_paren]
 
 class EventListenerThread(threading.Thread):
 
-	def run(self):
-		while True:
-			reply_id, flags, err, data = self.jdwp_.recv()
-			self.jdwp_.replies[reply_id] = (flags, err, data)
+  def run(self):
+    while True:
+      reply_id, flags, err, data = self.jdwp_.recv()
+      self.jdwp_.replies[reply_id] = (flags, err, data)
 
-ERROR_MESSAGE_LINES = [ line.strip().split("\t") for line in 
-    """0	NONE	No error has occurred.
+ERROR_MESSAGE_LINES = [ line.strip().split("\t") for line in \
+"""0	NONE	No error has occurred.
 10	INVALID_THREAD	Passed thread is null, is not a valid thread or has exited.
 11	INVALID_THREAD_GROUP	Thread group invalid.
 12	INVALID_PRIORITY	Invalid priority.
@@ -327,7 +326,7 @@ ERROR_MESSAGE_LINES = [ line.strip().split("\t") for line in
 67	DELETE_METHOD_NOT_IMPLEMENTED	The new class version does not declare a method declared in the old class version and canUnrestrictedlyRedefineClasses is false.
 68	UNSUPPORTED_VERSION	A class file has a version number not supported by this VM.
 69	NAMES_DONT_MATCH	The class name defined in the new class file is different from the name in the old class object.
-70	CLASS_MODIFIERS_CHANGE_NOT_IMPLEMENTED	The new class version has different modifiers and anUnrestrictedlyRedefineClasses	and	canUnrestrictedlyRedefineClasses is false.
+70	CLASS_MODIFIERS_CHANGE_NOT_IMPLEMENTED	The new class version has different modifiers and anUnrestrictedlyRedefineClasses and canUnrestrictedlyRedefineClasses is false.
 71	METHOD_MODIFIERS_CHANGE_NOT_IMPLEMENTED	A method in the new class version has different modifiers than its counterpart in the old class version and and canUnrestrictedlyRedefineClasses is false.
 99	NOT_IMPLEMENTED	The functionality is not implemented in this virtual machine.
 100	NULL_POINTER	Invalid pointer.

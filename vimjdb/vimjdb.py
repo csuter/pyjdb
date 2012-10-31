62171e43c18c39659bba8abd584fa1c0d6092593
Christopher Suter <cgs1019@gmail.com>
2012-10-31 01:28:47 -0700

    move vimjdb.py to use subprocess. start adding event support

--- vimjdb/vimjdb.py
+++ vimjdb/vimjdb.py
@@ -1,10 +1,13 @@
-import os, time
-import protobuf.socketrpc
 import jdwp_pb2
+import os
+import protobuf.socketrpc
+import subprocess
+import time
 
 channel = protobuf.socketrpc.channel.SocketRpcChannel("localhost", 10001)
 controller = channel.newController()
 virtual_machine_service = jdwp_pb2.VirtualMachine_Stub(channel)  
+event_request_service = jdwp_pb2.EventRequest_Stub(channel)
 
 class ResultProviderCallback:
   def __init__(self):
@@ -19,20 +22,19 @@ def do_sync(method, reqeuest):
   method(controller, reqeuest, callback)
   while not callback.has_result:
     time.sleep(.1)
-  print("got %s" % callback.result)
   return callback.result
 
 def VirtualMachine_Version():
   request = jdwp_pb2.VirtualMachine_Version_Request()
   return do_sync(virtual_machine_service.VirtualMachine_Version, request)
 
-def VirtualMachine_Threads():
-  """returns a tree of all thread groups and threads"""
-  request = jdwp_pb2.VirtualMachine_AllThreads_Request()
-  thread_ids = do_sync(virtual_machine_service.VirtualMachine_AllThreads, request)
-  print(thread_ids)
+def EventRequest_Set_Something():
+  request = jdwp_pb2.EventRequest_Set_Request()
+  request.eventKind = jdwp_pb2.EventKind_CLASS_LOAD
+  request.suspendPolicy = jdwp_pb2.SuspendPolicy_EVENT_THREAD
+  return do_sync(event_request_service.EventRequest_Set, request)
 
 def Jdwprpc_Spawn(jvm_port):
-  cmd = "build-bin/jdwprpc/jdwprpc.py"
-  argv = [cmd, 10001, jvm_port]
-  print("spawned: %s" % os.strerror(os.spawnvpe(os.P_WAIT, cmd, argv, {'PYTHONPATH':'build-bin/jdwprpc:build-bin/'})))
+  cmd = "%s/build-bin/jdwprpc/jdwprpc.sh" % os.getcwd()
+  subprocess.Popen([cmd, "10001", "5005"])
+  time.sleep(1)
